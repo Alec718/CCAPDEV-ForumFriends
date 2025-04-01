1791eeab2715cfe6368f1dc1710571ab897b053b
Alec718 <aamos_delacruz@dlsu.edu.ph>
2025-04-02 04:17:17 +0800

    New: About page
    Added about page with the project specifications
    
    Removed the debugging for profile route.

--- routes/rootRoutes.js
+++ routes/rootRoutes.js
@@ -6,5 +6,12 @@ router.get('/', (req, res) => {
   res.redirect('/login');
 });
 
+// About Route
+router.get('/about', (req, res) => {
+  res.render('about', {
+    title: "About - Forum Friends"
+  });
+});
+
 module.exports = router;
 
